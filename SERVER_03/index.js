2a37f6f97dd7b0b3ae7fdec17b76189911c4aaa2
Tran Diep <trandiep43tp.com>
2017-12-06 16:58:31 +0700

    node

--- SERVER_03/index.js
+++ SERVER_03/index.js
@@ -37,8 +37,17 @@ app.post("/login",urlencodedParser, function(req,res){
 	
 	
 	//cấu hình ejs
-	app.set("view engine", "ejs");  //khai báo
+	app.set("view engine", "ejs");  //khai báo template engine
 	app.set("views", "./views");    //khai báo thư mục chứa view
-	app.get("/trangchu", function(req,res){
+	app.get("/", function(req,res){
 		res.render("bai1");
 		});
+
+	app.get("/bai2", function(req,res){
+		res.render("bai2",{name:"Tran Diep"});
+	});
+
+	app.get("/bai2/:x", function(req,res){
+		var x=req.params.x;
+		res.render("bai2",{name:"Tran Diep",x:x,namsinh:[1987,1988,1989,1990]});
+	});
