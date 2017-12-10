70186c91c87ead0546dd1665cac95d3a5f41b42d
Tran Diep <contact@trandiep.com>
2017-12-10 23:07:30 +0700

    bai2

--- SERVER_04/index.js
+++ SERVER_04/index.js
@@ -8,10 +8,35 @@ app.set("view engine","ejs");
 app.set("views","./views");
 
 var server=require("http").createServer(app);
-server.listen(3000,console.log("ket noi thanh cong"));
+var io=require("socket.io")(server);          //khai báo socket.io
+server.listen(3000);
 
+//socket lắng nghe kết nối
+io.on("connection",function(socket){
+    console.log("co nguoi ket noi: " + socket.id);
+    socket.on("disconnect",function(){
+        console.log(socket.id + " ngat ket noi");
+    });
 
+    //nhận dữ liệu từ client
+    socket.on("client-send-data", function(data){
+        console.log(socket.id + " gui data: " + data);
 
+        //server chỉ gửi lại cho chính client gui lên. 
+        //socket.emit("server-send-data",data + "888");
+
+        //server gửi lại tất cả các client
+        //io.sockets.emit("server-send-data", data + "888");
+
+        //server gửi lại cho các client trừ client gửi lên
+        socket.broadcast.emit();
+        
+        //server gửi riêng cho 1 client nào đó
+        //io.to(socket.id).emit("server-send-data", data + "888");
+        
+    });
+
+});
 
 app.get("/", function(req,res){
     res.render("trangchu");
