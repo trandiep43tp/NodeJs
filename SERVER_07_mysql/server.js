17720047503245fd5c855d443fc4f8aae50fb927
trandiep43tp <trandiep43tp@gmail.com>
2018-01-07 23:29:28 +0700

    add server07 pgadmin

---
+++ SERVER_07_mysql/server.js
@@ -0,0 +1,48 @@
+var express= require("express");
+var app=express();
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+app.set("views","./views");
+
+var server = require("http").createServer(app);
+var io=require("socket.io")(server);
+server.listen(3000);
+
+
+
+app.get("/loaimonan", function(req,res){
+    var mysql= require("mysql");
+    //cấu hình kết nối
+    var conn= mysql.createConnection({
+        host    : 'localhost',
+        user    : 'root',
+        password: '',
+        database: 'ql_nha_hang'
+    });
+    //kết nối
+    conn.connect( function(err){
+            //nếu có lỗi thì in ra
+            if(err) throw err;
+            //nếu thành công
+            console.log("ket noi thanh cong");
+    });
+    //lấy dữ liệu từ mysql
+    var sql="SELECT * FROM loai_mon_an";
+    conn.query(sql, function(error,result){
+        if(error) throw error.stack;
+        //console.log(result);
+        res.render("loai_mon_an",{kq: result});        
+    });  
+    //ngắt kết nối
+    conn.end(function(err){
+        if(err) throw err;
+        console.log("ngat ket noi thanh cong");
+    });  
+});
+
+
+app.get("/", function(req,res){
+    res.render("trangchu");
+});
